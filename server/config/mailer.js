14448a511e9aa845dbbc0ac768e71a37b13ce296
LuxHedge Admin <admin@luxhedge.com>
2025-09-28 02:45:25 -0800

    fix: use correct frontend URL and update mailer for SendGrid support

--- server/config/mailer.js
+++ server/config/mailer.js
@@ -2,7 +2,8 @@ const nodemailer = require('nodemailer');
 require('dotenv').config({ path: require('path').resolve(__dirname, '../../.env') });
 
 const transporter = nodemailer.createTransport({
-  service: process.env.EMAIL_SERVICE, // 'gmail'
+  host: process.env.EMAIL_HOST,
+  port: process.env.EMAIL_PORT,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
