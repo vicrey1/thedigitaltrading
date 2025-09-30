d4831dce6a4f8c7b16b04f93714bbccef51cd053
LuxHedge Admin <admin@luxhedge.com>
2025-09-30 08:29:46 -0800

    fix: export API instance from adminAPI service

--- client/src/services/adminAPI.js
+++ client/src/services/adminAPI.js
@@ -1,7 +1,7 @@
 // src/services/adminAPI.js
 import axios from 'axios';
 
-const API = axios.create({
+export const API = axios.create({
   baseURL: process.env.REACT_APP_API_BASE_URL + '/api/admin',
 });
 
